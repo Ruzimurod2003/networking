195fcadfd86ae605dc54ea3192a6778cdd974fd8
Ruzimurod2003 <Ruzimurodabdunazarov2003@mail.ru>
2022-12-13 10:15:32 +0500

    initial

---
+++ HttpClientApp/Program.cs
@@ -0,0 +1,11 @@
+﻿using HttpClientApp.Main;
+
+//Functions.EnunciationIPAddress();
+//Functions.EnunciationIPEndpoint();
+//Functions.WorkWithUri();
+//Functions.GetHostEntry();
+//Functions.GetHosAddresses();
+//Functions.GetNetworkInterfaces();
+//Functions.GetAllDevicesInNetwork();
+//Functions.Monitoring();
+Functions.WorkWithSocket();
